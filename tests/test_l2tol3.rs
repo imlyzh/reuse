2224f40890dc5c48510b96e6ff89c84cd82995db
imlyzh <enterindex@gmail.com>
2024-06-07 17:05:12 +0800

    add testcase

--- tests/test_l2tol3.rs
+++ tests/test_l2tol3.rs
@@ -134,3 +134,60 @@ pub fn test_l2tol3_if() {
     let gen_f = f.linearize(&linears);
     println!("gen result:\n{}", gen_f);
 }
+
+#[test]
+pub fn test_l3_nested_if() {
+    let input_output_type = Type::Struct(StructType {
+        name: "Cons".to_owned(),
+        fields: vec![Type::Int, Type::Int],
+    });
+    let f = Function {
+        name: "test".to_string(),
+        return_type: Type::Int,
+        args: vec![
+            ("a".to_string(), (input_output_type.clone(), Owned::Linear)),
+            ("b".to_string(), (input_output_type.clone(), Owned::Linear)),
+        ],
+        body: Body::BindPattern(BindPattern {
+            pat: Pattern::Constructor(
+                "Cons".to_owned(),
+                vec![
+                    Pattern::Variable("x".to_string()),
+                    Pattern::Variable("xx".to_string()),
+                ],
+            ),
+            ty: input_output_type.clone(),
+            value: "a".to_string(),
+            cont: Box::new(Body::BindPattern(BindPattern {
+                pat: Pattern::Constructor(
+                    "Cons".to_owned(),
+                    vec![
+                        Pattern::Variable("y".to_string()),
+                        Pattern::Variable("yy".to_string()),
+                    ],
+                ),
+                ty: input_output_type.clone(),
+                value: "b".to_string(),
+                cont: Box::new(Body::If(If {
+                    cond: "x".to_owned(),
+                    then: Box::new(Body::If(If {
+                        cond: "y".to_owned(),
+                        then: Box::new(Body::Variable("x".to_owned())),
+                        else_: Box::new(Body::Variable("y".to_owned())),
+                    })),
+                    else_: Box::new(Body::If(If {
+                        cond: "y".to_owned(),
+                        then: Box::new(Body::Variable("xx".to_owned())),
+                        else_: Box::new(Body::Variable("yy".to_owned())),
+                    })),
+                })),
+            })),
+        }),
+    };
+
+    let mut used_record = Used::new();
+    f.use_check(&mut used_record).unwrap();
+    let linears = used_record.to_linears();
+    let gen_f = f.linearize(&linears);
+    println!("gen result:\n{}", gen_f);
+}
